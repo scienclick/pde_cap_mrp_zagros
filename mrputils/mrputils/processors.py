f57e76ccb02b0d991916850b3cd9ab6145b80555
Amir Shamsa <scienclick@gmail.com>
2022-12-06 04:31:01 -0700

    nltk added

--- mrputils/mrputils/processors.py
+++ mrputils/mrputils/processors.py
@@ -9,6 +9,7 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
 import nltk
+nltk.download('stopwords')
 stop_words = nltk.corpus.stopwords.words("english")
 lemmatizer = WordNetLemmatizer()
 from sklearn.feature_extraction.text import CountVectorizer
